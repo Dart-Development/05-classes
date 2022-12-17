14b0328bcf9e4708ce4dbc038736f3d6390891c5
bgarcia95 <bgarcia.95@outlook.com>
2022-12-17 12:34:00 -0600

    understand classes on different files and code optimization

--- 01_class.dart
+++ 01_class.dart
@@ -1,29 +1,14 @@
+import 'classes/Person.dart';
+
 main() {
-  // final person = {'name': 'Brayan', 'age': 27, 'bio': 'Was born in La Union'};
-
-  // print(person['age']);
-
   // recommended final if we are going to work with the defined properties and methods
-  final Person person = new Person();
+  final person = new Person();
 
-  person.name = 'Brayan';
-  person.age = 27;
-  person.bio = 'Was born somewhere';
+  person
+    ..name = 'Brayan'
+    ..age = 27
+    ..bio = 'Was born somewhere';
 
-  print(person.toString());
-}
-
-class Person {
-  // Properties
-  String? name;
-  int? age;
-  String? bio;
-
-  // Get & Sets
-
-  // Constructors
-
-  // Methods
-  @override
-  String toString() => '$name $age $bio';
+  // .toString() is implicit when using print
+  print(person);
 }
---
+++ classes/Person.dart
@@ -0,0 +1,14 @@
+class Person {
+  // Properties
+  String? name;
+  int? age;
+  String? bio;
+
+  // Get & Sets
+
+  // Constructors
+
+  // Methods
+  @override
+  String toString() => '$name $age $bio';
+}
